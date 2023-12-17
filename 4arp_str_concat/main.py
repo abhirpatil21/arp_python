9d83b3e2a87dbdc807fa74f3c10ca15ac6ca3663
abhirpatil21 <abhirpatil21@gmail.com>
2023-12-17 16:11:38 +0530

    added str concat main

---
+++ 4arp_str_concat/main.py
@@ -0,0 +1,7 @@
+#Program to display your full name
+
+first_name = input("Enter your first name : ")
+middle_name = input("Enter your middle name : ")
+last_name = input("Enter your last name : ")
+
+print("You full name :" + first_name + " " + middle_name + " " + last_name)
