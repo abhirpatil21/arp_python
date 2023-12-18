1148291d1c456f923551f33a045d50d9660901e2
abhirpatil21 <abhirpatil21@gmail.com>
2023-12-18 09:49:42 +0530

    oddeven main

---
+++ 7arp_oddeven/main.py
@@ -0,0 +1,9 @@
+#Find even odd number
+
+print("Find given number is even or odd")
+number = int(input("Enter the number : "))
+
+if number % 2 == 0:
+    print(f"{number} is Even")
+else:
+    print(f"{number} is Odd")
