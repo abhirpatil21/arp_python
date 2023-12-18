aa8ff9d4dd0081fc4598edbb9a3aad87058f74a2
abhirpatil21 <abhirpatil21@gmail.com>
2023-12-18 09:16:28 +0530

    added bmi main

---
+++ 6arp_bmi/main.py
@@ -0,0 +1,15 @@
+# bmi calculator taking input in feet and inch
+print("BMI Calculator - input feet and inch separate")
+kg = float(input("Enter your weight in Kgs : "))
+feet = int(input("Input feet : "))
+inch = float(input("Input inch : "))
+
+#f_mtr = feet * 0.3048
+#i_mtr = ( 1.0/12 ) * 0.3048 * inch
+#in_meter = f_mtr + i_mtr
+height_in_meter = (feet * 0.3048) + ((1.0/12) * 0.3048 * inch)
+
+#bmi calculation
+bmi = kg / height_in_meter ** 2
+
+print(f"Your BMI is : {bmi}")
